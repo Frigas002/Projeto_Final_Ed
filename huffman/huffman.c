e3254f19d7d2d32f672502e8c253d2e786620eba
Matheus Campos <matheuskalleb1908@gmail.com>
2026-06-16 21:39:07 -0300

    adicionando uns comentarios

--- huffman/huffman.c
+++ huffman/huffman.c
@@ -12,7 +12,7 @@
 typedef struct arve {
     void* caracter; 
     int frequencia;
-    struct arve *esq, *dir, *prox;
+    struct arve *esq, *dir, *prox; // faz o no da arvore ter dupla utilidade (serve pra ser colocado na lista encadeada tbm)
 } arve;
 
 /**
@@ -93,7 +93,7 @@ void init(unsigned int tab[]) {
 
 arve* remover(Lista *lista) {
     arve *aux = NULL;
-    if(lista->inicio) {
+    if(lista->inicio) { //checa se eh diferente de null
         aux = (arve *)lista->inicio;
         lista->inicio = aux->prox;
         aux->prox = NULL;
@@ -257,7 +257,7 @@ void compactar(char** dicionario, unsigned int tab[], arve *arvore, const char *
                 i++;
             }
         }
-        if(j != 7) {
+        if(j != 7) { //se o j continuar valendo 7 significa que o ultimo byte foi gravado perfeitamente, se n quer dizer que falta um pedaco p escrever do ultimo byte
             fwrite(&byte, sizeof(unsigned char), 1, out);
         }
         
@@ -274,20 +274,20 @@ arve* reconstruir_arvore(FILE *arq, int *tree_size) {
     if (*tree_size <= 0) return NULL;
     
     unsigned char c;
-    fread(&c, sizeof(unsigned char), 1, arq);
-    (*tree_size)--;
+    fread(&c, sizeof(unsigned char), 1, arq); //le o char
+    (*tree_size)--; 
     
     arve *novo = malloc(sizeof(arve));
-    unsigned char *dado = malloc(sizeof(unsigned char));
+    unsigned char *dado = malloc(sizeof(unsigned char)); //malloc pro dado ja que ele ta como void
     
-    if (c == '*') {
+    if (c == '*') { //no interno
         *dado = '*';
         novo->caracter = dado;
         novo->esq = reconstruir_arvore(arq, tree_size);
         novo->dir = reconstruir_arvore(arq, tree_size);
-    } else {
+    } else { //folha
         if (c == '\\') { // Se for escape, lê o próximo como caractere real
-            fread(&c, sizeof(unsigned char), 1, arq);
+            fread(&c, sizeof(unsigned char), 1, arq); 
             (*tree_size)--;
         }
         *dado = c;
